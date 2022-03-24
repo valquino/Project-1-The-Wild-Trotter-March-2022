7f2cfa1c20c942eff35219ed857849244edaf5af
Diseb <sebastiangomes77@gmail.com>
2022-03-24 16:38:48 +0100

    add homepage index.php

---
+++ index.php
@@ -0,0 +1,65 @@
+<?php include("header.php"); ?>
+
+    <main> 
+      <section id="banner">
+        <div><h1>The Wild Trotter</h1></div>
+        <div id="about">
+          <h2>Qui suis-je ? </h2>
+          <p> Régulièrement au bout du monde, J'explore des terres inconnues. Je vous embarquerais dans des destinations au coeur de paysages insoupçonnés.
+          </p>
+          <p> La Terre regorge de paysage et d'animaux sauvages. Je vous propose quelques séjours loin de chez vous pour observer et suivre les traces de ces animaux et ainsi vivre une expérience inoubliable dans la vie sauvage.
+          </p>
+        </div>
+      </section>
+      <section id="main-section">
+        <section id="essentials">
+          <div> 
+            <h2>Les essentiels </h2>
+          </div> 
+          <div id="icon-essentiels">
+            <div class="flip-card">
+              <div class="flip-card-inner">
+                <div class="flip-card-front">
+                  <i class="fa-brands fa-safari"></i>
+                </div>
+                <div class="flip-card-back">
+                  <p>Etape 1: Choisir la destination de son choix</p> 
+                </div>
+              </div>
+            </div>   
+            <div class="flip-card">
+              <div class="flip-card-inner">
+                <div class="flip-card-front">
+                  <i class="fa-solid fa-suitcase"></i>
+                </div>
+                <div class="flip-card-back">
+                  <p>Etape 2: Faites vos sacs</p>
+                </div>
+              </div>
+            </div>   
+            <div class="flip-card">
+              <div class="flip-card-inner">
+                <div class="flip-card-front">
+                  <i class="fa-solid fa-face-smile"></i>
+                </div>
+                <div class="flip-card-back">
+                  <p>Etape 3:      Let's go!!</p>
+                </div>
+              </div>
+            </div>   
+          </div>
+        </section>
+        <section id="top-destination">
+          <div>
+            <h2>Top 3 Destinations Wild </h2>
+          </div>
+          <div id="galeries-photos-small">
+            <div id="amazonie"><div class="layer">Amazonie</div></div>
+            <div id="alaska"><div class="layer">Alaska</div></div>
+            <div id="tanzanie"><div class="layer">Tanzanie</div></div>
+          </div>
+        </section>
+      <section>
+    </main>
+
+    <?php include("footer.php"); ?>
