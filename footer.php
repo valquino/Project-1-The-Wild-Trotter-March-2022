c859cba5e3420834ca3bb97ca2ed6cae3ae870b8
titiphenom-wcs <valentino-quinola_student2022@wilder.school>
2022-03-24 16:03:18 +0100

    original files of the project

---
+++ footer.php
@@ -0,0 +1,31 @@
+    <div id="wrap">
+      <div class="header">
+        <nav class="nav">
+          <a href="#wrap" id="open">
+            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="34px" height="27px" viewBox="0 0 34 27" enable-background="new 0 0 34 27" xml:space="preserve">
+              <rect fill="#FFFFFF" width="34" height="4"/>
+              <rect y="11" fill="#FFFFFF" width="34" height="4"/>
+              <rect y="23" fill="#FFFFFF" width="34" height="4"/>
+            </svg>
+          </a>
+          <a href="#" id="close">×</a>
+          <ul>
+            <li><a href="index.php"><i class="fa-solid fa-house-user"></i>Accueil</a></li>
+            <li><a href="destinations.php"><i class="fa-solid fa-map-location-dot"></i>Destinations</a></li>
+            <li><a href="galeries-photos.php"><i class="fa-solid fa-camera-retro"></i>Galerie photos</a></li>
+            <li><a href="reservations.php"><i class="fa-solid fa-file-contract"></i>Réservations</a></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+    
+    <footer>
+      <div>The Wild Trotter &copy 2022. Tous droits reservés</div>
+      <ul class="socialnetworks">
+        <li><a href="#"><i class="fa-brands fa-facebook"></i></a></li>
+        <li><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
+        <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
+      </ul>
+    </footer>
+  </body>
+</html>
---
+++ header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>The Wild Trotter</title>
+    <link href="style.css" rel="stylesheet">
+    <link href="responsive.css" rel="stylesheet">
+    <link href="lightbox.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/d3f484778a.js" crossorigin="anonymous"></script>
+    <script src="functions.js"></script>
+  </head>
+  <body>
+  
+    <header>
+      <nav id="menu-principal">
+        <ul id="menu">
+          <li><a href="index.php"><i class="fa-solid fa-house-user"></i>Accueil</a></li>
+          <li><a href="destinations.php"><i class="fa-solid fa-map-location-dot"></i>Destinations</a></li>
+          <li><a href="galeries-photos.php"><i class="fa-solid fa-camera-retro"></i>Galerie photos</a></li>
+          <li><a href="reservations.php"><i class="fa-solid fa-file-contract"></i>Réservations</a></li>
+        </ul>
+        <ul class="socialnetworks">
+          <li><a href="#"><i class="fa-brands fa-facebook"></i></a></li>
+          <li><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
+          <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
+        </ul>
+      </nav>
+    </header>
