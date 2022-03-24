b20f5f26a242d505e05902cf9b841b2b60fe0ac9
Somida-KAMISSOKO <somidak@hotmail.fr>
2022-03-24 16:49:27 +0100

    add destination.php

---
+++ destinations.php
@@ -0,0 +1,102 @@
+<?php include("header.php"); ?>
+      
+<main id="content">      
+
+<h2 id="titre-destination">Quelques destinations de rêve</h2>
+   
+      <section class="photos-descriptions">
+
+      <nav id="menu-destination">
+        <ul>
+          <li><a href="#lien-alaska">Alaska</a></li>
+          <li><a href="#lien-amazonie">Amazonie</a></li>
+          <li><a href="#lien-laponie">Laponie</a></li>
+          <li><a href="#lien-nepal">Népal</a></li>
+          <li><a href="#lien-tanzanie">Tanzanie</a></li>
+          <li><a href="#lien-tasmanie">Tasmanie</a></li>
+      </ul>
+    </nav>
+
+          <div id="alaska" class="medium">
+            <h3 id="lien-alaska">Alaska</h3>
+          </div>
+
+          <div class="descriptions">
+              <p>Un voyage en Alaska, c’est un rêve pour tout voyageur 
+                  amateur de grands espaces et de terres lointaines. 
+                  C’est un territoire immense, mêlant paysages aussi 
+                  incroyables que variés et refuge d’une faune exceptionnelle, 
+                  les grizzlys en tête. Découvrez la nature primitive du Denali 
+                  Park et des glaciers immaculés du Mc Kinley, admirez les champs 
+                  de glace de Wrangell Saint-Elias et rejoignez les eaux limpides 
+                  de prince William Sound. Enfin, partez à la rencontre des orques 
+                  dans les Kenai Fjords lors d’excursions en bateau. Dans cet immense 
+                  territoire situé sur le cercle arctique, l’été, la nuit n’existe quasi 
+                  plus. Un voyage en Alaska c’est aussi une rencontre avec inuits, sur 
+                  les traces des chercheurs d’or et des explorateurs.
+                  <br><br><a href="galeries-photos.php">voir photos</a></p>
+          </div>
+
+          <div id="amazonie" class="medium">
+          <h3 id="lien-amazonie">Amazonie</h3>
+          </div>
+
+          <div class="descriptions">
+              <p>Un voyage en Amazonie, c’est le rêve absolu du voyageur aventurier. Un mythe 
+                  absolu. Une démesure à l’échelle du Brésil. Un fleuve puissant qui s’écoule 
+                  sur 6 000 km et qui reçoit plus de 1 100 affluents… Ce gigantesque poumon vert, 
+                  qui couvre plus de 40 % du Brésil, est la plus grande réserve écologique de la 
+                  planète. Une forêt tropicale où vivent des arbres géants qui luttent pour gagner 
+                  leur paradis : la lumière. Au cœur de ce « Brésil vert », des villes, des villages 
+                  et des gens qui vivent. Ici le fleuve c’est la vie. Alors l’ambiance dans ces ports 
+                  du bout du monde est unique. Les Urubus guettent les rives et des crieurs haranguent 
+                  les éventuels passagers des bateaux taxis. Belem, Marajo, Santarem, Manaus … Des lodges 
+                  sont installés le long du Rio Negro et les bateaux de croisières naviguent dans les Igarapés.
+                  <br><br><a href="galeries-photos.php">voir photos</a></p>
+          </div>
+
+          <div id="laponie" class="medium">
+          <h3 id="lien-laponie">Laponie</h3>
+          </div>
+
+          <div class="descriptions">
+              <p>«Le pays du bout », aux portes des glaces de l’Arctique. La Laponie. Si loin du monde, que le Père Noël a trouvé bon de s’y installer, sans doute en gage de tranquillité ! Quel espace envoûtant, ces immenses terres à perte de vue pelotonnées l’hiver dans leur manteau blanc et si éclatantes de vie aux beaux jours. Dans la pénombre de la nuit, l’hiver s’installe dans la durée. Le Grand Nord ouaté a bien des atouts – crissement des raquettes, roulades dans la poudreuse, chiens de traineaux haletants, aurores boréales comme des mirages dans le ciel étoilé…un rêve de voyage imaginaire qui taquine notre sens de la réalité.
+                <br><br><a href="galeries-photos.php">voir photos</a>
+              </p>
+          </div>
+
+          <div id="nepal" class="medium">
+          <h3 id="lien-nepal">Népal</h3>
+          </div>
+
+          <div class="descriptions">
+              <p>Voyage au Népal ou voyage au milieu des plus hauts cieux du monde ! Au début on n’y croit pas. Il fait chaud, on marche au sein des bananiers, on nous a trompé, ce n’est pas de sitôt qu’on sera près des névés! Pourtant on voit déjà les sommets enneigés, mais ils semblent irréels, décoratifs, comme les posters géants de tulipes hollandaises vus dans quelques gargotes lors du séjour à Katmandou ou à Bodnath, à chacun sa vision de paradis. Il suffit de quelques jours de trek et les ethnies sont montagnardes, le Tibet proche se fait sentir, d’un coup la vallée s’ouvre et l’Annapurna III dévoile ses 7755 mètres de roche et de glace aigues, on est plongé dans la pureté
+                <br><br><a href="galeries-photos.php">voir photos</a>
+              </p>        
+            </div>
+
+          <div id="tanzanie" class="medium">
+          <h3 id="lien-tanzanie">Tanzanie</h3>
+          </div>
+
+          <div class="descriptions">
+              <p>Le temps d’un voyage en Tanzanie, partir à la rencontre d’un continent et d’un océan… Il y a le spectacle de l’aube tout en nuances orangées et les nuits sous la tente, comme un prolongement des émotions de l’enfance. Les savanes immenses du Serengeti, le cratère de Ngorongoro, la beauté rose du lac Manyara, ses flamands et pélicans. Les baobabs et les éléphants du Tarangire, les crocodiles et les hippos du Ruaha. Et puis sur l’océan Indien, l’archipel de Zanzibar. Le gris d’un ponton se fond dans le bleu du ciel, le sable blond strié rappelle le pelage des zèbres. On se perd dans le lacis de ruelles et d’effluves d’épices de la vieille ville de Stone Town, tandis que les massifs coralliens nous révèlent avec un masque et un tuba autant de splendeurs qu’ailleurs en plongée profonde... 
+                <br><br><a href="galeries-photos.php">voir photos</a>
+              </p>
+          </div>
+
+          <div id="tasmanie" class="medium">
+          <h3 id="lien-tasmanie">Tasmanie</h3>
+          </div>
+
+          <div class="descriptions"> 
+            <p>Voyager en Tasmanie, le plus petit État d’Australie, vaut parce qu’elle offre plus de diversité de paysages qu’aucune autre région du pays. Réputée pour son hospitalité, l’île est également appréciée pour sa gastronomie, mais c’est avant tout sa nature quasi intacte et sauvage qui l’a rendue célèbre car un tiers du territoire est composé de parcs nationaux. Son climat est doux et maritime avec des étés tempérés et des hivers neigeux sur les hauteurs, assez froids. 
+              <br><br><a href="galeries-photos.php">voir photos</a>
+            </p>
+          </div>
+          <br>
+          <button><a href="#titre-destination"> Retour en haut </a></button>
+      </section>
+    </main>
+
+    <?php include("footer.php"); ?>
